90fe407658753c40c9fcf33f84d2c282664eaf15
Brandon Kilpatrick <blkilpatrick42@gmail.com>
2024-05-22 16:01:28 -0500

    rudimentary interiors

---
+++ scenes/exterior_tilemap.gd
@@ -0,0 +1,25 @@
+extends TileMap
+@onready var _transparency_area = $Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
+
+func _on_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_entered(body):
+	if(body.is_in_group("player")):
+		visible = false
+
+
+func _on_area_2d_body_exited(body):
+	if(body.is_in_group("player")):
+		visible = true
