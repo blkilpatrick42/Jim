41e37b5dc40058d6b7a8804d47463b4745e2ff16
Brandon Kilpatrick <blkilpatrick42@gmail.com>
2024-06-08 21:56:31 -0500

    add menu scene transition

--- scripts/start screen/menu_manager.gd
+++ scripts/start screen/menu_manager.gd
@@ -6,7 +6,10 @@ extends Node
 func _ready():
 	pass # Replace with function body.
 
+func get_input():
+	if Input.is_action_just_pressed("start"):
+		get_tree().change_scene_to_file("res://scenes/dev_base_3.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	get_input()
