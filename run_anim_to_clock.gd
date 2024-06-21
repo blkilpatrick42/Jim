15b097bfe8703a71693a84a789ba4cf2079a8e23
Brandon Kilpatrick <blkilpatrick42@gmail.com>
2024-06-21 02:13:43 -0500

    added buildings, pizza building, new windows, neon pizza sign

--- run_anim_to_clock.gd
+++ run_anim_to_clock.gd
@@ -1,4 +1,4 @@
-extends PointLight2D
+extends AnimatedSprite2D
 
 @export var turns_on = 0
 @export var turns_off = 23
@@ -8,13 +8,12 @@ var time_keeper = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	enabled = false
 	time_keeper = get_tree().get_first_node_in_group("time_keeper")
-
+	play("off")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(time_keeper.clock == turns_on):
-		enabled = true
+		play("running")
 	elif(time_keeper.clock == turns_off):
-		enabled = false
+		play("off")
