cdff80d56af0569b5599a68ab8357c4826c8aa76
Brandon Kilpatrick <blkilpatrick42@gmail.com>
2024-07-11 18:05:37 -0500

    mob sprite offset adjusts while they are knocked out pt 2

--- scripts/AI_state_machine/states/mobster/state_dead.gd
+++ scripts/AI_state_machine/states/mobster/state_dead.gd
@@ -6,8 +6,10 @@ signal disable_collision()
 signal animate(animation : String)
 signal die_skull()
 signal blood()
+signal adjust_offset(adjustment : Vector2)
 var timer := Timer.new() 
 var disappear_time_secs = 60
+var sprite_offset_amt = 16
 
 func process(_delta: float) -> void:
 	pass
@@ -19,6 +21,7 @@ func physics_process(_delta: float) -> void:
 func enter(_msg := {}) -> void:
 	animate.emit(str("fallen_",ai_state_machine.perceptions.facing_dir))
 	disable_collision.emit()
+	adjust_offset.emit(Vector2(0,sprite_offset_amt))
 	die_skull.emit()
 	blood.emit()
 	timer.one_shot = true
