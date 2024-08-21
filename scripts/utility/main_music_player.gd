cf507f13b3b2a34348123b1325e19577d310cbfa
Brandon Kilpatrick <blkilpatrick42@gmail.com>
2024-08-21 16:15:46 -0500

    add music zones

---
+++ scripts/utility/main_music_player.gd
@@ -0,0 +1,51 @@
+extends AudioStreamPlayer
+
+var current_stream : String = ""
+
+var current_volume = 0
+var zero_volume = -60
+
+var timer : Timer = Timer.new()
+var fade_step = 3
+var fade_step_time = 0.05
+#var start_new_stream_wait_time = 1
+
+var changing_streams = false
+#var ready_play_new_stream = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.one_shot = true
+	add_child(timer)
+	bus = "Music"
+
+func change_stream(new_stream: String):
+	#if(current_stream != ""):
+	changing_streams = true
+	current_stream = new_stream
+	timer.start(fade_step_time)
+	#else:
+		#current_volume = 0
+		#changing_streams = false
+		#current_stream = new_stream
+		#stream = load(new_stream)
+		#play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(changing_streams && timer.is_stopped()):
+		if(current_volume > zero_volume):
+			current_volume = current_volume - fade_step
+			volume_db = current_volume
+			timer.start(fade_step_time)
+		elif(current_volume <= zero_volume):
+			stop()
+			changing_streams = false
+			if(current_stream != ""):
+				current_volume = 0
+				volume_db = current_volume
+				stream = load(current_stream)
+				play()
+				
+			
+			
---
+++ scripts/utility/music_zone.gd
@@ -0,0 +1,45 @@
+extends Node
+@export var song_deck : Array[String]
+
+var time_keeper = null
+var main_music_player = null
+var internal_clock = 0
+
+var active = false
+
+func get_song_deck():
+	return song_deck
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	time_keeper = get_tree().get_first_node_in_group("time_keeper")
+	main_music_player = get_tree().get_first_node_in_group("main_music_player")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(internal_clock != time_keeper.clock &&
+	active == true):
+		internal_clock = time_keeper.clock
+		if(song_deck.size() > 1 &&
+		song_deck[internal_clock] != ""):
+			var new_stream = get_song_deck()[internal_clock]
+			main_music_player.change_stream(new_stream)
+		elif(song_deck.size() == 1):
+			var new_stream = get_song_deck()[0]
+			main_music_player.change_stream(new_stream)
+			
+
+func _on_body_entered(body : Node):
+	if(body.is_in_group("player") and not active):
+		active = true
+		var new_stream
+		if(song_deck.size() > 1):
+			new_stream = get_song_deck()[internal_clock]
+		else:
+			new_stream = get_song_deck()[0] 
+		main_music_player.change_stream(new_stream)
+
+func _on_body_exited(body : Node):
+	if(body.is_in_group("player") and active):
+		main_music_player.change_stream("")
+		active = false
