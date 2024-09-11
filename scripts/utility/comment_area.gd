9535f49484cfcc13a898fbf069b0cdf63a565b2d
Brandon Kilpatrick <blkilpatrick42@gmail.com>
2024-09-11 16:00:41 -0500

    added movement-triggered comment areas

--- scripts/utility/comment_area.gd
+++ scripts/utility/comment_area.gd
@@ -1,15 +1,31 @@
-extends StaticBody2D
+extends CollisionObject2D
 
 signal make_comment(String)
 
 @export var text : String
+
+#two kinds, one for interacting and one for playing text when the player
+#walks into the zone
+@export var text_when_zone_entered : bool = false
+@export var play_once : bool = false
+var has_played = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func interact():
-	make_comment.emit(text)
+	if(!text_when_zone_entered):
+		make_comment.emit(text)
+
+func _on_body_entered(body : Node2D):
+	if(body.is_in_group("player")):
+		if(text_when_zone_entered):
+			if(play_once && !has_played):
+				has_played = true
+				make_comment.emit(text)
+			elif(!play_once):
+				make_comment.emit(text)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
