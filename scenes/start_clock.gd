813a537813fea6563918869624df05b31ffce679
blkilpatrick42 <blkilpatrick42@gmail.com>
2024-09-20 09:50:33 -0500

    music for entrance tunnel

---
+++ scenes/start_clock.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+func run_script():
+	var time_keeper = get_tree().get_first_node_in_group("time_keeper")
+	time_keeper.unlock_time()
--- scripts/utility/comment_area.gd
+++ scripts/utility/comment_area.gd
@@ -7,6 +7,7 @@ signal make_comment(String)
 #two kinds, one for interacting and one for playing text when the player
 #walks into the zone
 @export var text_when_zone_entered : bool = false
+@export var script_when_zone_entered : bool = false
 @export var play_once : bool = false
 var has_played = false
 
@@ -24,8 +25,13 @@ func _on_body_entered(body : Node2D):
 			if(play_once && !has_played):
 				has_played = true
 				make_comment.emit(text)
+				if(script_when_zone_entered && get_children().size() > 0):
+					get_children()[0].run_script()
 			elif(!play_once):
 				make_comment.emit(text)
+				if(script_when_zone_entered && get_children().size() > 0):
+					get_children()[0].run_script()
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
